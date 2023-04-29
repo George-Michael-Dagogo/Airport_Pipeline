5eef167b9e2e5bcacd47147e9bc3eb05f9ab7107
GeeKaizer <georgemichaeldagogo@gmail.com>
2023-04-29 14:19:20 +0000

    geonames data gotten but too big for github

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 import os
 path = "../Airport_Pipeline/Airport_data"
+path2 = "../Airport_Pipeline/Geonames_data"
 for i in os.listdir(path):
     os.remove(path +'/' +i)
+
+for k in os.listdir(path2):
+    os.remove(path2 +'/' + k)
     
-os.system('wget -P ../Airport_Pipeline/Airport_data https://davidmegginson.github.io/ourairports-data/airports.csv')
-os.system('wget -P ../Airport_Pipeline/Airport_data https://davidmegginson.github.io/ourairports-data/airport-frequencies.csv')
-os.system('wget -P ../Airport_Pipeline/Airport_data https://davidmegginson.github.io/ourairports-data/airport-comments.csv')
-os.system('wget -P ../Airport_Pipeline/Airport_data https://davidmegginson.github.io/ourairports-data/runways.csv')
-os.system('wget -P ../Airport_Pipeline/Airport_data https://davidmegginson.github.io/ourairports-data/navaids.csv')
-os.system('wget -P ../Airport_Pipeline/Airport_data https://davidmegginson.github.io/ourairports-data/countries.csv')
-os.system('wget -P ../Airport_Pipeline/Airport_data https://davidmegginson.github.io/ourairports-data/regions.csv')
-#/Airport_Pipeline/Airport_data
+os.system('wget -P ../Airport_Pipeline/Airport_data -i airport.txt')
+#os.system('wget -P ../Airport_Pipeline/Geonames_data -i geonames.txt')
+
+
+
---
+++ test.py
@@ -0,0 +1,3 @@
+import os 
+#wget -i example.txt
+os.system('wget -P ../Airport_Pipeline/Geonames_data -i geonames.txt')
