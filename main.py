5c3cb315ae17ce603683ba842ae0db990d705b50
GeeKaizer <georgemichaeldagogo@gmail.com>
2023-06-01 12:51:28 +0000

    automation arranged

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import os
 import zipfile
+from pyspark.sql import SparkSession
 path = "../Airport_Pipeline/Airport_data"
 path2 = "../Airport_Pipeline/Geonames_data"
+path3 = '../Airport_Pipeline/Geonames_data/countries.csv'
+
 for i in os.listdir(path):
     os.remove(path +'/' + i)
 
@@ -16,3 +19,21 @@ for k in os.listdir(path2):
         with zipfile.ZipFile(path2+ '/'+k) as zf:
             zf.extractall(path2)
         os.remove(path2+ '/'+k)
+
+spark = SparkSession.builder.appName("ReadTextFile").getOrCreate()
+header_names = ["geoname_id", "name", "asciiname","alternatenames","latitude","longitude",
+    "feature_class","feature_code","country_code","altcountry_code","admin1_code","admin2_code"
+    ,"admin3_code","admin4_code","population","elevation","DEM","timezone","modification_date" ]
+path4 = "/workspace/Airport_Pipeline/Geonames_data/allCountries.txt"
+
+
+
+df = spark.read.option("header", "true").option("delimiter", "\t").csv(path4).toDF(*header_names)
+
+df.coalesce(1).write.mode("overwrite").option("header", "true").csv("../Airport_Pipeline/Geonames_data/countries.csv")
+
+for l in os.listdir(path3):
+    if l.endswith('.csv'):
+        os.rename(path3 + '/'+ l,'../Airport_Pipeline/Geonames_data/all_countries.csv')
+
+
--- test.py
+++ test.py
@@ -1,22 +1,19 @@
-from snowflake.sqlalchemy import URL 
-from sqlalchemy import create_engine 
+from pyspark.sql import SparkSession
+import os
 
-engine = create_engine(URL( #https://plninim-tg58176.snowflakecomputing.com
-                          user= 'GEORGE9042',
-                          account='plninim-tg58176',
-                          #region='switzerland-north.azure',
-                          password = 'George9042',
-                          warehouse='COMPUTE_WH',
-                          database='MY_TEST', 
-                          schema='NEW',
-                          role='ACCOUNTADMIN'
-                          ))
+spark = SparkSession.builder.appName("ReadTextFile").getOrCreate()
+header_names = ["geoname_id", "name", "asciiname","alternatenames","latitude","longitude",
+    "feature_class","feature_code","country_code","altcountry_code","admin1_code","admin2_code"
+    ,"admin3_code","admin4_code","population","elevation","DEM","timezone","modification_date" ]
+path4 = "/workspace/Airport_Pipeline/Geonames_data/allCountries.txt"
 
 
-connection = engine.connect()
-try:
-    connection.execute('select * from airports')
-finally:
-    connection.close()
-    engine.dispose()
-       
+
+df = spark.read.option("header", "true").option("delimiter", "\t").csv(path4).toDF(*header_names)
+
+df.coalesce(1).write.mode("overwrite").option("header", "true").csv("/workspace/Airport_Pipeline/Geonames_data/countries.csv")
+
+for k in os.listdir(path3):
+    if k.endswith('.csv'):
+        os.rename(path3 + '/'+ k,'../Airport_Pipeline/Geonames_data/all_countries.csv')
+
